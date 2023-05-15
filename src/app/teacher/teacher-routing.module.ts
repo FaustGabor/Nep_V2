b52e481ebd140bcf7f27fbfdf185f521b727f67c
FaustGabor <57292763+FaustGabor@users.noreply.github.com>
2023-05-15 17:38:56 +0200

    2 files modified

--- src/app/teacher/teacher-routing.module.ts
+++ src/app/teacher/teacher-routing.module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
             component: TeacherListComponent,
           },
           {
-            path: 'list_subject',
+            path: 'list_subject/:teacherId',
             component: TeacherListSubjectComponent,
           },
           {
