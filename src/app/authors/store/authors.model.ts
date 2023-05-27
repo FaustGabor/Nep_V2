8e2fc9d98ac471fc56cb42e98bd1df5a66f7418c
FaustGabor <57292763+FaustGabor@users.noreply.github.com>
2023-05-27 13:26:44 +0200

    62 files deleted

--- src/app/authors/store/authors.model.ts
+++
@@ -1,9 +0,0 @@
-import { Author } from "../../data/authors.data";
-
-export class AuthorModel implements Author {
-  id: number;
-  name: string;
-  birthYear: number;
-  nationality: string;
-  deleted: boolean;
-}
